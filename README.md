7d47d49b8de211417159969d2c07d252eb7a5c1e
Jeanmarie <jeanmarie@dynamicalperspectives.com>
2017-07-25 14:52:33 -0400

    Stuff I worked on today

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# whiskersJMtest
-This was cloned from Sam's test files.  It was NOT forked from our org repo. This is standalone.
+# whiskers
+Final Project
+
+18 July 2017 - JM Note: Testing my remote connection to the repo.
---
+++ models/randomizer.js
@@ -0,0 +1,21 @@
+
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+let RandomPetSchema = mongoose.Schema({
+  age: String,
+  city: String,
+  description: String,
+  email: String,
+  image: String,
+  name: String,
+  sex: String,
+  size: String,
+  state: String,
+  type: String
+});
+
+var RandomPet = mongoose.model("RandomPets", RandomPetSchema);
+
+module.exports = RandomPet;
+
